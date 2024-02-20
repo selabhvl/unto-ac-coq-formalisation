8c86d19b4a488776ea516feb9375745cae96bbad
Ulises Nicolas Torrella <ulisestorrella@Ulisess-MacBook-Pro.local>
2024-02-20 14:16:12 +0100

    for some reason device_tac in bs test doesn't work if it isn't in the same file

--- test/big_step_semantics_test.v
+++ test/big_step_semantics_test.v
@@ -33,6 +33,43 @@ Module BS_Test (M: MAPPING).
   -split. apply ordered2. auto. apply ordered1. simpl. auto.
   Qed.
   
+  Ltac device_tac :=
+    repeat (
+      simpl;auto;
+    first [ 
+      eapply A_MULT;idtac "MULT"
+    
+    | eapply A_UID;idtac "UID"
+    | eapply A_FOLD;idtac "FOLD"
+    | eapply A_SELF;idtac "SELF"
+    | eapply A_EXCHANGE;idtac "EXCHANGE"
+    | eapply A_OR;idtac "OR"
+    | eapply A_AND;idtac "OR"
+    
+    | eapply E_NVAL;idtac "NVAL"
+    | eapply A_SENS;idtac "SENS"
+    | eapply E_LIT;idtac "LIT"
+    | eapply E_VAR;idtac "VAR"
+    
+    | eapply A_FUN;idtac "FUN"
+    
+    | eapply E_APP_3;idtac "APP_3"
+    | eapply E_APP_2;idtac "APP_2"
+    | eapply E_APP;idtac "APP"
+    | eapply E_VAL;idtac "VAL"
+    
+    | eapply A_SELF;idtac "SELF"
+    |
+    (match goal with 
+    | [|- w_value ?X] =>idtac "W_TAC"; w_tac
+    | [|- value ?X] => idtac "VALUE";unfold value; simpl; repeat split; auto
+    | [|- is_fun ?X] => idtac "FUNC"; apply func + apply built
+    | [|- bigstep (Input ?id ?s ?v_env (exp_nvalue ?w)) (Output (exp_nvalue ?w) (empty nil) )] => idtac "GOP"
+    | _ => fail
+    end
+    )
+    ]).
+
   Lemma fold00: exists w, <[ 4 | base_sens | vt_el 2 (empty nil) (vt_el 3 (empty nil) (vt_end)) | <{app nfold $([> fun fun0[x] {fun fun0[y] {app mult $x y$} }]) ([2>>4][3>>5][>6]) ([>7])$}> ]> ==> <[ <{w}> | empty nil ]>.
   Proof.
   eexists.
